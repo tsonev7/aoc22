caaf7caea46e8071cf77e26739ef542611c09136
Dimitar Tsonev <dimitar@st6.io>
2022-12-08 16:43:28 +0200

    Optimize solution

--- day1/first.c
+++ day1/first.c
@@ -11,21 +11,12 @@ int main()
     int maxNumber = 0;
     int currentNumber = 0;
 
-    // Read the first gorup of numbers and set that to be the max number
     pFile = fopen("input", "r");
-    while (strcmp(buffer, "\n"))
-    {
-        fgets(buffer, MAX_LEN, pFile);
-        maxNumber += atoi(buffer);
-    };
-
-    // Read the rest of the groups and compare each group to the maxNumber
     while (fgets(buffer, MAX_LEN, pFile))
     {
         currentNumber += atoi(buffer);
         if (!strcmp(buffer, "\n"))
         {
-            printf("Current number: %d, Max number: %d \n", currentNumber, maxNumber);
             if (maxNumber < currentNumber)
             {
                 maxNumber = currentNumber;
